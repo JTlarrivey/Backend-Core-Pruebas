9f102039e8127c6652d9844d4cd20b8ca1504543
Larry <joaqlarrivey@gmail.com>
2025-09-29 14:32:37 -0300

    a ver ahora

--- public/index.php
+++ public/index.php
@@ -1,22 +1,8 @@
 <?php
 
 declare(strict_types=1);
-
-if (!defined('APP_ROOT')) {
-    define('APP_ROOT', dirname(__DIR__));
-}
+if (!defined('APP_ROOT')) define('APP_ROOT', dirname(__DIR__));
 require APP_ROOT . '/app/autoload.php';
-
-// En DEV mostramos errores; en PROD los mandamos al log
-if ((getenv('APP_ENV') ?: 'prod') !== 'prod') {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', '0');
-    ini_set('log_errors', '1');
-    error_reporting(E_ALL);
-}
 
 use App\Router\Router;
 
